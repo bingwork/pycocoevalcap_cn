03655716ae5311b9b29158e8b4ead79b42d2562b
BillBao <billbao1993@gmail.com>
2024-03-14 06:19:07 +0000

    add scripts

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+ 
+setup(
+    name='pycocoevalcap_cn',
+    version='0.0.1', 
+    description='pycocoevalcap_cn',  
+    author='bingwork', 
+    author_email='bingwork@qq.com',
+)
